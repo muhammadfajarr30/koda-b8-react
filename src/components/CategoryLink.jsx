31f21a547d8f78c68c6c18a397fee8a097868d6c
muhammadfajarr30 <muhammad.fajarr362@gmail.com>
2026-06-14 13:54:02 +0700

    feat: add category link components for landing page

---
+++ src/components/CategoryLink.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+const CategoryLink = ({ idx, title, amount, thumbnail }) => {
+  return (
+    <Link
+      to={`/products/${thumbnail}`}
+      className={`category category-${idx} flex flex-col bg-white border border-[#0000001A] rounded-xl items-center px-2 py-6`}>
+      <img
+        className="w-16 rounded-xl"
+        src={`/images/${thumbnail}.png`}
+        alt=""
+      />
+      <p>{title}</p>
+      <p className="text-gray">{amount} produk</p>
+    </Link>
+  );
+};
+
+export default CategoryLink;
