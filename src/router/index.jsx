0a480a4e0f6ec551a702f8058915eb899119429d
muhammadfajarr30 <muhammad.fajarr362@gmail.com>
2026-06-15 01:09:33 +0700

    feat: add browsepage route

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,10 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import LandingPage from "../pages/product/LandingPage";
+import BrowsePage from "../pages/product/BrowsePage";
+import MainLayout from "/src/layouts/MainLayout";
+
 
 export const router = createBrowserRouter([
   {
+    children: [{ path: "/", element: <App /> }],
+  },
+  {
+    element: <MainLayout />,
     children: [
-      { path: "/", element: <App /> },
+      { path: "/products", element: <LandingPage /> },
+      { path: "/browse", element: <BrowsePage /> },
     ],
   },
 ]);
+
