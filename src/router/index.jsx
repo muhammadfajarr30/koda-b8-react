e1d07b1b0fdaf31bbfb8a65ab570d4a67c38404c
muhammadfajarr30 <muhammad.fajarr362@gmail.com>
2026-06-16 00:45:02 +0700

    feat: add detail page routing

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -3,7 +3,7 @@ import App from "../App";
 import LandingPage from "../pages/product/LandingPage";
 import BrowsePage from "../pages/product/BrowsePage";
 import MainLayout from "/src/layouts/MainLayout";
-
+import DetailPage from "../pages/product/DetailPage";
 
 export const router = createBrowserRouter([
   {
@@ -14,6 +14,7 @@ export const router = createBrowserRouter([
     children: [
       { path: "/products", element: <LandingPage /> },
       { path: "/browse", element: <BrowsePage /> },
+      { path: "/detail-page", element: <DetailPage /> },
     ],
   },
 ]);
