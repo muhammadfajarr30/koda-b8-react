a458a3eecb9d47cb4386469a8a06bc83db878eb5
muhammadfajarr30 <muhammad.fajarr362@gmail.com>
2026-06-16 15:33:34 +0700

    feat: add path to checkout payment page

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,21 +1,46 @@
+// import App from "../App";
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
 import LandingPage from "../pages/product/LandingPage";
 import BrowsePage from "../pages/product/BrowsePage";
 import MainLayout from "/src/layouts/MainLayout";
 import DetailPage from "../pages/product/DetailPage";
+import CartPage from "../pages/product/CartPage";
+import LoginPage from "../pages/auth/LoginPage";
+import CheckoutLayout from "../layouts/CheckoutLayout";
+import AddressPage from "../pages/checkout/AddressPage";
+import PaymentPage from "../pages/checkout/PaymentPage";
 
 export const router = createBrowserRouter([
-  {
-    children: [{ path: "/", element: <App /> }],
-  },
+  // {
+  //   children: [{ path: "/", element: <App /> }],
+  // },
   {
     element: <MainLayout />,
     children: [
-      { path: "/products", element: <LandingPage /> },
+      { path: "/", element: <LandingPage /> },
       { path: "/browse", element: <BrowsePage /> },
       { path: "/detail-page", element: <DetailPage /> },
+      { path: "/cart", element: <CartPage /> },
+    ],
+  },
+
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+
+  {
+    element: <CheckoutLayout />,
+    path: "/checkout",
+    children: [
+      {
+        path: "address",
+        element: <AddressPage/>
+      },
+      {
+        path: "payment",
+        element: <PaymentPage/>
+      }
     ],
   },
 ]);
-
