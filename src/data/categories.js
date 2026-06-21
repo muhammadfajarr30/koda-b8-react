1d2ab0e55f419b684e366ec0b80259628b5b42af
muhammadfajarr30 <muhammad.fajarr362@gmail.com>
2026-06-21 21:40:40 +0700

    feat : add data categories for landing page category section

---
+++ src/data/categories.js
@@ -0,0 +1,13 @@
+import { products } from "./products";
+
+export const categories = [
+  { slug: "elektronik", title: "Elektronik", thumbnail: "electronic" },
+  { slug: "fashion", title: "Fashion", thumbnail: "fashion" },
+  { slug: "rumah-dapur", title: "Rumah & Dapur", thumbnail: "house-kitchen" },
+  { slug: "kecantikan", title: "Kecantikan", thumbnail: "beauty" },
+  { slug: "olahraga", title: "Olahraga", thumbnail: "sport" },
+  { slug: "buku-alat-tulis", title: "Buku & Alat Tulis", thumbnail: "book" },
+].map((category) => ({
+  ...category,
+  amount: products.filter((p) => p.category === category.slug).length,
+}));
