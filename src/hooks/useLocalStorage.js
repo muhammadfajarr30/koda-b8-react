c3c0075a389c5debebcdad4bfc2ea9f93d41b506
muhammadfajarr30 <muhammad.fajarr362@gmail.com>
2026-06-26 14:05:22 +0700

    feat: add initialValue parameter

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,13 +1,19 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
-export default function useLocalStorage(key) {
-    const [data, setData] = useState(() => {
-        const stored = localStorage.getItem(key);
-        return stored ? JSON.parse(stored) : [];
-    });
+export default function useLocalStorage(key, initialValue) {
+  const [data, setData] = useState(() => {
+    const stored = localStorage.getItem(key);
 
-    useEffect(()=>{
-        window.localStorage.setItem(key, JSON.stringify(data))  
-    },[key, data])
-    return [data, setData]
+    if (stored) {
+      return JSON.parse(stored);
+    }
+
+    return initialValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(data));
+  }, [key, data]);
+
+  return [data, setData];
 }
