dd208e1f6c736335536709d12824d64748161e84
muhammadfajarr30 <muhammad.fajarr362@gmail.com>
2026-06-26 14:20:27 +0700

    feat: add payment method data dinamically and implement react hook from

--- src/pages/checkout/PaymentPage.jsx
+++ src/pages/checkout/PaymentPage.jsx
@@ -1,81 +1,113 @@
 import { CreditCard, Lock } from "lucide-react";
 import React from "react";
+import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import { useState } from 'react'
+
+const paymentMethods = [
+  { id: "bca", label: "Virtual Account BCA" },
+  { id: "bni", label: "Virtual Account BNI" },
+  { id: "credit-card", label: "Kartu Kredit / Debit" },
+  { id: "gopay", label: "Gopay" },
+  { id: "ovo", label: "Ovo" },
+  { id: "dana", label: "Dana" },
+];
 
 const PaymentPage = () => {
   const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      payment: "",
+    },
+  });
+
+  const [user, setUser] = useState(() => {
+    const data = localStorage.getItem("account");
+    if (!data) return null;
+    const account = JSON.parse(data);
+    return account;
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    const updateUser = { 
+      ...user, 
+      checkout : { 
+        ...user.checkout, 
+        payment: data.payment,
+      }
+    }
+
+    setUser(updateUser); 
+    localStorage.setItem("account", JSON.stringify(updateUser));
+    navigate("/checkout/confirm-order");
+  };
+
   return (
     <div>
       <section className="payment-method bg-white p-4 rounded-xl border border-gray-300">
         <div className="container flex flex-col gap-4">
-          <div className=" flex items-center gap-3 title">
+          <div className="flex items-center gap-3">
             <CreditCard color="#1A73E8" />
             <h3 className="font-semibold text-2xl">Metode Pembayaran</h3>
           </div>
-          <div className="payment-choice grid grid-cols-3 gap-4">
-            <div className="flex items-center justify-center gap-3 border p-3 rounded-xl border-gray-300">
-              <input type="radio" name="payment" id="bca" />
-              <label htmlFor="bca" className="payment-card">
-                {/* <img src="" alt="" /> */}
-                <span>Virtual Account BCA</span>
-              </label>
-            </div>
-            <div className="flex items-center justify-center gap-3 border p-3 rounded-xl border-gray-300">
-              <input type="radio" name="payment" id="bni" />
-              <label htmlFor="bni" className="payment-card">
-                {/* <img src="" alt="" /> */}
-                <span>Virtual Account BNI</span>
-              </label>
-            </div>
-            <div className="flex items-center justify-center gap-3 border p-3 rounded-xl border-gray-300">
-              <input type="radio" name="payment" id="credit-card" />
-              <label htmlFor="credit-card" className="payment-card">
-                {/* <img src="" alt="" /> */}
-                <span>Kartu Kredit / Debit</span>
-              </label>
-            </div>
-            <div className="flex items-center justify-center gap-3 border p-3 rounded-xl border-gray-300">
-              <input type="radio" name="payment" id="gopay" />
-              <label htmlFor="gopay" className="payment-card">
-                {/* <img src="" alt="" /> */}
-                <span>Gopay</span>
-              </label>
-            </div>
-            <div className="flex items-center justify-center gap-3 border p-3 rounded-xl border-gray-300">
-              <input type="radio" name="payment" id="ovo" />
-              <label htmlFor="ovo" className="payment-card">
-                {/* <img src="" alt="" /> */}
-                <span>Ovo</span>
-              </label>
-            </div>
-            <div className="flex items-center justify-center gap-3 border p-3 rounded-xl border-gray-300">
-              <input type="radio" name="payment" id="dana" />
-              <label htmlFor="dana" className="payment-card">
-                {/* <img src="" alt="" /> */}
-                <span>Dana</span>
-              </label>
-            </div>
-          </div>
 
-          <div className="security-info bg-blue-100 p-3 rounded-xl text-gray-500 flex gap-2 items-center justify-center">
-            <Lock color="#1A73E8" size={20} />
-            <p>
-              Informasi pembayaranmu dienkripsi dengan SSL 256-bit. Kami tidak
-              menyimpan data kartu kreditmu.
-            </p>
-          </div>
-          <div className="return-confirm flex gap-2">
-            <button
-              onClick={() => navigate(-1)}
-              className="btn-return p-3 border border-gray-300 rounded-xl  text-gray-500">
-              Kembali
-            </button>
-            <button
-              onClick={() => navigate("/checkout/confirm-order")}
-              className="btn-confirm p-3 w-full bg-blue-500  rounded-xl text-white">
-              Lanjut Ke Konfirmasi
-            </button>
-          </div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="payment-choice grid grid-cols-3 gap-4">
+              {paymentMethods.map((method) => (
+                <div
+                  key={method.id}
+                  className="flex items-center justify-center gap-3 border p-3 rounded-xl border-gray-300">
+                  <input
+                    type="radio"
+                    id={method.id}
+                    value={method.id}
+                    {...register("payment", {
+                      required: "Pilih metode pembayaran",
+                    })}
+                  />
+
+                  <label
+                    htmlFor={method.id}
+                    className="payment-card cursor-pointer">
+                    <span>{method.label}</span>
+                  </label>
+                </div>
+              ))}
+            </div>
+
+            {errors.payment && (
+              <p className="text-red-500 mt-2">{errors.payment.message}</p>
+            )}
+
+            <div className="security-info bg-blue-100 p-3 rounded-xl text-gray-500 flex gap-2 items-center justify-center mt-4">
+              <Lock color="#1A73E8" size={20} />
+              <p>
+                Informasi pembayaranmu dienkripsi dengan SSL 256-bit. Kami tidak
+                menyimpan data kartu kreditmu.
+              </p>
+            </div>
+
+            <div className="return-confirm flex gap-2 mt-4">
+              <button
+                type="button"
+                onClick={() => navigate(-1)}
+                className="btn-return p-3 border border-gray-300 rounded-xl text-gray-500">
+                Kembali
+              </button>
+
+              <button
+                type="submit"
+                className="btn-confirm p-3 w-full bg-blue-500 rounded-xl text-white">
+                Lanjut Ke Konfirmasi
+              </button>
+            </div>
+          </form>
         </div>
       </section>
     </div>
