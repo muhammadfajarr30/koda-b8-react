8416d6fb4f93d37572ed7cfa855c8aa6be7215e6
muhammadfajarr30 <muhammad.fajarr362@gmail.com>
2026-06-29 11:24:47 +0700

    feat: add AdminLayout component for admin interface structure

---
+++ src/layouts/AdminLayout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const AdminLayout = () => {
+  return (
+    <div>
+        <aside>
+            
+        </aside>
+        <div>
+            <header>
+                
+            </header>
+        </div>
+    </div>
+  )
+}
+
+export default AdminLayout
